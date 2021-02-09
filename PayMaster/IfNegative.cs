8b6a9ef835b1e8a1ca8c8e50f2909cb460fb83a6
RAF <rafal.michal.rogowski@gmail.com>
2021-02-09 14:34:24 +0100

    changes

--- PayMaster/IfNegative.cs
+++ PayMaster/IfNegative.cs
@@ -10,7 +10,7 @@ using System.Windows.Media;
 namespace PayMaster
 {
 
-    public class IfNegative : IValueConverter
+    class IfNegative : IValueConverter
     {
         
             public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
